e598799fbc5bbfcc64c7735fa07d70394bdb5488
Matt Diephouse <matt@diephouse.com>
2010-01-10 14:33:11 -0500

    Add an --output-dir command-line option

--- Source/preggers.m
+++ Source/preggers.m
@@ -15,22 +15,30 @@
 #include <getopt.h>
 
 static const char *usage_string =
-"preggers [--version] [--help] file\n";
+"preggers [--version] [--help] [--output-dir dir] [-d dir] file\n";
 
 static int opt_version;
 static int opt_help;
 static struct option longopts[] = {
-    { "version",    no_argument, &opt_version, 1 },
-    { "help",       no_argument, &opt_help,    1 },
-    { NULL,         0,           NULL,         0 }
+    { "version",    no_argument,        &opt_version,       1 },
+    { "help",       no_argument,        &opt_help,          1 },
+    { "output-dir", required_argument,  NULL,               'd'},
+    { NULL,         0,                  NULL,               0 }
 };
 
 int main (int argc, char *argv[])
 {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    NSString *outputDir;
     int ch;
-    while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1)
+    while ((ch = getopt_long(argc, argv, "d:", longopts, NULL)) != -1)
         switch (ch)
         {
+            case 'd':
+                outputDir = [[[NSString stringWithUTF8String:optarg]
+                              stringByExpandingTildeInPath] stringByStandardizingPath];
+                break;
             case 0:
                 if (opt_version)
                 {
@@ -52,15 +60,13 @@ int main (int argc, char *argv[])
     argc -= optind;
     argv += optind;
     
-    if (argc != 2)
+    if (argc != 1)
     {
         printf("Usage: %s", usage_string);
         return 1;
     }
     
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    
-    NSString *path = [NSString stringWithUTF8String:argv[1]];
+    NSString *path = [NSString stringWithUTF8String:argv[0]];
     if (![path isAbsolutePath])
         path = [[[NSFileManager defaultManager]
                  currentDirectoryPath] stringByAppendingPathComponent:path];
@@ -83,8 +89,11 @@ int main (int argc, char *argv[])
     {
         NSString *fileWithoutExtension = [path stringByDeletingPathExtension];
         compiler.className  = [fileWithoutExtension lastPathComponent];
-        compiler.headerPath = [fileWithoutExtension stringByAppendingPathExtension:@"h"];
-        compiler.sourcePath = [fileWithoutExtension stringByAppendingPathExtension:@"m"];
+        if (!outputDir)
+            outputDir = [fileWithoutExtension stringByDeletingLastPathComponent];
+        outputDir = [outputDir stringByAppendingPathComponent:compiler.className];
+        compiler.headerPath = [outputDir stringByAppendingPathExtension:@"h"];
+        compiler.sourcePath = [outputDir stringByAppendingPathExtension:@"m"];
         [compiler compile];
     }
     else
